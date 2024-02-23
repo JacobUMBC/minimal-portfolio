82b817fca560f183ab9b5899e35102f44609f539
JacobUMBC <jacobr5@umbc.edu>
2024-02-23 11:36:31 -0500

    Second change

--- src/data/timeline.js
+++ src/data/timeline.js
@@ -1,32 +1,42 @@
 export default [
+    
+    {
+        year: "2023",
+        title: "B.S Information Systems and Cybersecurity Certfication",
+        duration: "August 2019 - Dec 2023",
+        details: "Bachelor of Science in Information Systems and also copmleted specialization in cybersecurity which included 4 cyber security classes. Dean's list of Honors for Spring 2023",
+    },
+    {
+        year: "2023",
+        title: "Data Analyst/Full Stack Developer",
+        duration: "6 months",
+        details: "Utilize Web Scraping  java based tools to store data into database Troubleshooting  pgAdmin queries and validated data in the right place. Worked on UX/UI with Figma and designed  web pages for the company itself. Trained students on Figma and UX/UI design.",
+        
+    },
+    {
+        year: "2021",
+        title: "Chairman of UMBC CyberDawgs",
+        duration: "2 years",
+        details: "Participating in different cybersecurity events such as penetration testing, capture the flag events and also decision making on new board members and events outside the club.", 
+        
+    },
     {
         year: "2020",
-        title: "Awana Leader",
+        title: "Secretary of UMBC Game Development Club",
         duration: "2 years",
-        details: "kids ministry",
+        details: "Was able to schedule appointments and special events for students, communicated effectively with the club Also was able to work on a game using Visual Studio code and was able to get GUI interface system to work",
     },
     {
-        year: "Today",
-        title: "Awana Leader",
+        year: "2019",
+        title: "Information Technology Support Specialist",
         duration: "2 years",
-        details: "kids ministry",
+        details: "Demonstrate the ability to install, configure and troubleshoot around computer hardware and software applications Developed tested and implemented and maintained software applications such as Ansible",
+        
     },
     {
-        year: "Today",
+        year: "2018",
         title: "Awana Leader",
-        duration: "2 years",
-        details: "kids ministry",
-    },
-    {
-        year: "Today",
-        title: "Awana Leader",
-        duration: "2 years",
-        details: "kids ministry",
-    },
-    {
-        year: "Today",
-        title: "Awana Leader",
-        duration: "2 years",
-        details: "kids ministry",
+        duration: "2018 - Current",
+        details: "Experienced Awana leader with a passion for nurturing children's spiritual and personal growth. Patient and compassionate, I create engaging environments that foster enthusiasm and promote learning. Highly organized, I effectively plan and coordinate various activities and events. With strong communication skills, I connect with children and parents, providing guidance and support. A creative thinker, I employ innovative teaching methods to make learning enjoyable and memorable. Committed to my own spiritual growth, I serve as a faithful role model. Collaborative and open-minded, I work well in a team and value diverse perspectives. Always eager to learn and grow, I continuously seek opportunities for personal and professional development",
     }
 ]
